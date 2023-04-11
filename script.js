2fd2ee4b6debf2e376c381729460a766daf46c42
Sumit Dutta <129226126+SumitDutta007@users.noreply.github.com>
2023-04-11 23:59:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -39,10 +39,11 @@ const option2 = document.querySelector('#option2');
 const option3 = document.querySelector('#option3');
 const option4 = document.querySelector('#option4');
 const submit=document.querySelector('#submit');
-
+const Submit = document.getElementsByClassName(".buton");
 const answers = document.querySelectorAll('.answer');
-
 const showScore = document.querySelector('#showScore');
+
+let buton=document.getElementsByClassName(".buton");
 
 let questionNumber = 0;
 let score = 0;
@@ -68,6 +69,10 @@ const getCheckedAnswer = ()=>{
     return answer;
 };
 
+const deselectAll = () =>{
+    answers.forEach((curAnsElem) => curAnsElem.checked = false);
+}
+
 submit.addEventListener('click',()=>{
     const checkedAnswer = getCheckedAnswer();
     console.log(checkedAnswer);
@@ -77,12 +82,20 @@ submit.addEventListener('click',()=>{
         score++;
     };
     questionNumber++;
+    deselectAll();
     if(questionNumber < quiz.length){
         loadQuestion();
     }
     else{
         showScore.innerHTML = `
-        <h3> You scored ${score}/${quiz.length}</h3>
-        <button class = "btn" onclick="location.reload()"></button>`;
+        <h3> You saved ${score}/${quiz.length} fishes!</h3>`
+        submit.innerText = "Play Again ;)";
+        submit.addEventListener('click',()=>{
+            location.reload();
+        });
+    
+        
+        showScore.display= "flex"; 
     }
-})
+});
+
